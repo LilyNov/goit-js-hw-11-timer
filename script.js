ef3cb277b116e1fcd19f9a2c235f8a83e8bac501
Liliia Novikova <kplily85@gmail.com>
2020-10-25 12:30:03 +0200

    created countDownTimer

--- script.js
+++ script.js
@@ -1,29 +1,72 @@
-new CountdownTimer({
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]')
+}
+
+class CountdownTimer {
+  
+  constructor({selector, targetDate}) {
+    this.intervalId = null;
+    this.selector = selector;
+    this.targetDate = targetDate;
+   }
+ 
+
+countTimer() {
+    const startTime = this.targetDate;
+
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now()
+      const countTime = startTime - currentTime
+      const time = this.getTimeComponents(countTime)
+      updateClockface(time)
+
+      if (countTime <= 0) {
+        clearInterval(this.intervalId)
+        const time = this.getTimeComponents(0)
+        updateClockface(time)
+      }
+     
+    }, 1000);
+  
+  }
+
+    getTimeComponents(time) {
+
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+      
+      return {days, hours, mins, secs}
+      }
+  
+  /*
+   * Принимает число, приводит к строке и добавляет в начало 0 если число меньше 2-х знаков
+   */
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+
+const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+  targetDate: new Date('Jan 01, 2021'),
+  //targetDate: new Date('Oct 25, 2020'),
+
 });
 
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+timer.countTimer()
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+function updateClockface({days, hours, mins, secs}) {
+  refs.days.textContent = days; 
+  refs.hours.textContent = hours; 
+  refs.mins.textContent = mins; 
+  refs.secs.textContent = secs; 
+  return `${days}:${hours}:${mins}:${secs}`
+}
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
